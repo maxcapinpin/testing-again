3ea92d9a80b8191c03a7e9203f27a18258e9536f
Max Capinpin <maxcapinpin@hotmail.com>
2019-05-10 15:18:14 -0600

    end of 5/10/2019

--- max-capinpin-react-portfolio/src/components/app.js
+++ max-capinpin-react-portfolio/src/components/app.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import moment from "moment"
 
+import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from "./navigation/navigation-component";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Jordan Hudgens Portfolio</h1>
+        <NavigationContainer />
+        <h1>Max Capinpin Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+        <PortfolioContainer />
       </div>
     );
   }
